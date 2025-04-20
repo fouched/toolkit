42476ba2fcf2b0b0b735bec5bef073a5a0e17e95
Fouche du Preez <fouche@limehouse.co.za>
2025-04-20 21:09:33 +0200

    removed go-validator dependency

--- v2/validator.go
+++ v2/validator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -96,20 +97,17 @@ func (v *Validation) IsDateISO(field Field) {
 	}
 }
 
-// go-validator wrappers below
-
 // IsEmail checks if a Field contains a valid email and adds an error if result is false
 func (v *Validation) IsEmail(field Field) {
-	//TODO - remove govalidator dep
-	//if !govalidator.IsEmail(field.Value) {
-	//	v.AddError(field.Name, fmt.Sprintf("%s must be a valid email address", field.Label))
-	//}
+	rxEmail := regexp.MustCompile("^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_` w2`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$")
+	if !rxEmail.MatchString(field.Value) {
+		v.AddError(field.Name, fmt.Sprintf("%s must be a valid email address", field.Label))
+	}
 }
 
 // NoSpaces checks if a Field contains spaces and adds an error if result is false
 func (v *Validation) NoSpaces(field Field) {
-	// TODO - remove govalidator dep
-	//if govalidator.HasWhitespace(field.Value) {
-	//	v.AddError(field.Name, fmt.Sprintf("%s does not allow spaces", field.Label))
-	//}
+	if strings.Contains(" ", field.Value) {
+		v.AddError(field.Name, fmt.Sprintf("%s does not allow any spaces", field.Label))
+	}
 }
